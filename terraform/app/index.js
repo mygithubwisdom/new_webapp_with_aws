0a38af65c5f396c1f45d6035cb28ec178b9beaae
mygithubwisdom <hovafella@yahoo.com>
2025-12-22 07:45:21 +0100

    terraform/app/index.js

--- terraform/app/index.js
+++ terraform/app/index.js
@@ -54,9 +54,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Something went wrong!' });
 });
 
-// Start server
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running and accessible at http://0.0.0.0:${PORT}`);
 });
 
 // Graceful shutdown
