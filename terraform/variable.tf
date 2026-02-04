b5dccccdd0231f851f44b0233a069e66c6d4c67a
mygithubwisdom <hovafella@yahoo.com>
2026-02-04 19:04:32 +0100

    docs: update .gitignore to exclude terraform plan files

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -69,14 +69,13 @@ variable "map_public_ip_on_launch" {
 variable "allowed_ssh_cidr" {
   description = "CIDR block allowed for SSH access"
   type        = string
-  default     = "41.217.79.245/32"
+  default     = "197.242.112.93/32"
 
   validation {
     condition     = can(cidrhost(var.allowed_ssh_cidr, 0))
     error_message = "allowed_ssh_cidr must be a valid CIDR (e.g. 203.0.113.10/32)."
   }
 }
-
 
 variable "public_nacl_name" {
   description = "Name for the public subnet NACL"
@@ -117,7 +116,7 @@ variable "https_port" {
 variable "SSH_laptop_ip" {
   description = "The public IP address of your laptop in CIDR notation"
   type        = string
-  default     = "41.217.79.245/32"
+  default     = "197.242.112.93/32"
 
   validation {
     condition     = can(cidrhost(var.SSH_laptop_ip, 0))
