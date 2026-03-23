ab06ae596cf9906e3ebafafcb3f00625a179e573
mygithubwisdom <hovafella@yahoo.com>
2026-03-23 08:18:10 +0100

    fix: align backend dynamodb table name and add WAF variables

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-aws-webapp-prod-state-1669w"
-    key     = "terraform/state/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-    dynamodb_table = "terraform-prod-state-lock"
+    bucket         = "terraform-aws-webapp-prod-state-1669w"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-aws-webapp-prod-state-lock"
   }
 }
