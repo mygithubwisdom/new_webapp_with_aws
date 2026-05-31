80189bca3e6f1378bc3ce57ca4bf53f5a8ce56f9
mygithubwisdom <hovafella@yahoo.com>
2026-05-31 23:58:09 +0100

    amend deploh.sh

--- terraform/scripts/deploy.sh
+++ terraform/scripts/deploy.sh
@@ -1,44 +1,40 @@
-locals {
-  user_data = <<-EOF
-    #!/bin/bash
-    set -e
+#!/bin/bash
+set -e
     
-    #1. System setup
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-    apt-get update
-    apt-get install -y nodejs git awscli
+#1. System setup
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt-get update
+apt-get install -y nodejs git awscli
     
-    # 2. App directory setup
-    mkdir -p /home/ubuntu/app
-    chown -R ubuntu:ubuntu /home/ubuntu/app
+# 2. App directory setup
+mkdir -p /home/ubuntu/app
+chown -R ubuntu:ubuntu /home/ubuntu/app
 
-    # Fix ownership
-    chmod 755 /home/ubuntu/app
-    cd /home/ubuntu/app
+# Fix ownership
+chmod 755 /home/ubuntu/app
+cd /home/ubuntu/app
     
-    # 3. Clone repo and setup app
-    sudo -u ubuntu git clone -b production https://github.com/mygithubwisdom/new_webapp_with_aws.git  .
+# 3. Clone repo and setup app
+sudo -u ubuntu git clone -b production https://github.com/mygithubwisdom/new_webapp_with_aws.git  .
  
-    # 4. Move into actual Node app folder 
-    cd terraform/app
+# 4. Move into actual Node app folder 
+cd terraform/app
     
-    # 5. Install PM2 and start the app
-    sudo -u ubuntu npm install --production
-    npm install -g pm2
+# 5. Install PM2 and start the app
+sudo -u ubuntu npm install --production
+npm install -g pm2
     
-    # 6. Environment variables
-    export PORT=${node_app_port}
-    export DATABASE_URL='postgresql://${db_username}:${db_password}@${db_endpoint}/${db_name}'
+# 6. Environment variables
+export PORT=${node_app_port}
+export DATABASE_URL='postgresql://${db_username}:${db_password}@${db_endpoint}/${db_name}'
     
-    # Persist variables
-    echo "export PORT=${node_app_port}" | sudo -u ubuntu tee -a /home/ubuntu/.bashrc > /dev/null
-    echo "export DATABASE_URL='postgresql://${db_username}:${db_password}@${db_endpoint}/${db_name}'" | sudo -u ubuntu tee -a /home/ubuntu/.bashrc > /dev/null
+# Persist variables
+echo "export PORT=${node_app_port}" | sudo -u ubuntu tee -a /home/ubuntu/.bashrc > /dev/null
+echo "export DATABASE_URL='postgresql://${db_username}:${db_password}@${db_endpoint}/${db_name}'" | sudo -u ubuntu tee -a /home/ubuntu/.bashrc > /dev/null
     
-    # 8. Start app with PM2
-    sudo -u ubuntu pm2 start index.js --name node-app
-    sudo -u ubuntu pm2 save
-    sudo -u ubuntu pm2 startup systemd -u ubuntu --hp /home/ubuntu
+# 8. Start app with PM2
+sudo -u ubuntu pm2 start index.js --name node-app
+sudo -u ubuntu pm2 save
+sudo -u ubuntu pm2 startup systemd -u ubuntu --hp /home/ubuntu
     
-    echo "Deployment complete"
-  EOF
-}
+echo "Deployment complete"
